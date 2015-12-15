64f2343c207b753237f1537529fe6135f0043c44
jxie007 <jxie007@e.ntu.edu.sg>
2015-12-15 20:56:56 +0800

    add database table 'likes'

--- app/Http/Controllers/functionLinksController.php
+++ app/Http/Controllers/functionLinksController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Tweet;
+use App\Like;
 use App\Http\Controllers\Controller;
 use Redirect;
 
@@ -14,24 +15,39 @@ class functionLinksController extends Controller
     public function like(Request $request)
     {
     	$user_id = $request->get('user_id');
-
         $tweet_id = $request->get('tweet_id');
+        $me = $request->session()->get('me_id');
 
-        // check how many 'like' for this twitter
-        $tweet = Tweet::where('id', $tweet_id)->first();
-        $like = $tweet->like;
+		// avoid one person gives more than one 'like' on the same tweet
+   		$count = Like::where('tweet_id', $tweet_id)
+   						->where('user_id' , $me)
+   						->count();
+   		if($count == 0)
+   		{
+	        // record this 'like' in database table 'likes'
+	        $likey = new Like;
+	        $likey->tweet_id = $tweet_id;
+	        $likey->user_id = $me; // If user_id equals 15, it means the user 
+	        					   // of id 15 have pressed the like button 
+	        					   // on this tweet
+	        $likey->save();
 
-        if(empty($like))
- 		{
-            $like = 1;
-        }
-        else
-        {
-            $like += 1;
-        }
+	        // add this 'like' in database table 'tweets'
+	        $tweet = Tweet::where('id', $tweet_id)->first();
+	        $like = $tweet->like;
 
-        $tweet->like = $like;
-        $tweet->save();
+	        if(empty($like))
+	 		{
+	            $like = 1;
+	        }
+	        else
+	        {
+	            $like += 1;
+	        }
+
+	        $tweet->like = $like;
+	        $tweet->save();
+   		}
 
         return Redirect::to('profile?user_id=' . $user_id);
     }
---
+++ database/migrations/2015_12_15_085937_create_likes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->timestamps();
+            $table->integer('tweet_id');
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('likes');
+        $table->dropColumn('tweet_id');
+        $table->dropColumn('user_id');
+    }
+}
