c1441b2f11aaf25c39f0edc7ac9955809edb1c0c
jxie007 <jxie007@e.ntu.edu.sg>
2015-12-24 19:15:15 +0800

    use as less parents as possible to style the document

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,44 +10,44 @@
 <body>
 	<header>
 		<div class="container">
-		<div class="nav">
-			<ul>
-				<li>					
-					<a href="{{ url('profile?user_id=' . session('me_id')) }}">
-						<span class="icon-home3"></span>
-						<span class="text_home">Home</span>
-					</a>
-				</li>
-				<li>						
-					<a href="#">
-						<span class="icon-heart"></span>
-						<span class="text_heart">Favorites</span>		
-					</a>
-				</li>				
-			</ul>
-		</div>
-		
-		<h1 class="icon-quill icon_header_center"></h1>
+			<div class="nav">
+				<ul>
+					<li>					
+						<a href="{{ url('profile?user_id=' . session('me_id')) }}">
+							<span class="icon-home3"></span>
+							<span class="text_home">Home</span>
+						</a>
+					</li>
+					<li>						
+						<a href="#">
+							<span class="icon-heart"></span>
+							<span class="text_heart">Favorites</span>	
+						</a>
+					</li>				
+				</ul>
+			</div>
+			
+			<h1 class="icon-quill icon_header_center"></h1>
 
-		<div class="header_right">
-			<div class="global_search_wrapper">
+			<div class="header_right">
 				<form action="#" class="global_search_form">
 					<input type="text" name="global_search" class="global_search_input" placeholder="Search Twitter">
 					<input type="submit" value="Go" class="global_search_submit">
 				</form>
-			</div>
-			
-			<div class="global_profile_wrapper">
-				<a href="{{ url('profile_and_settings') }}"><img src="
-				{{ $me->pro_img_path }}" alt="Photograph of {{ $me->name }}" class="global_profile">
+				
+				<a class="global_profile" href="{{ url('profile_and_settings') }}">
+					<img src="
+					{{ $me->pro_img_path }}" alt="Photograph of {{ $me->name }}" class="global_profile_img">
 				</a>			
-			</div>
-			
-			<div class="global_tweet_wrapper">
-				<a href="{{ url('sendTwitter') }}"><button class="global_tweet"><span class="icon-compose icon"></span>Tweet</button></a>
+		
+				<a class=" global_tweet" href="{{ url('sendTwitter') }}">
+					<button class="global_tweet_button">
+						<span class="icon-compose"></span>
+						<span class="text_tweet">Tweet</span>
+					</button>
+				</a>
 			</div>	
-		</div>
-		</div>		
+		</div>	
 	</header>
 
     @yield('content')
