0a008cd36da13aa7d04e9c9c64243925ee8cf9dd
jxie007 <jxie007@e.ntu.edu.sg>
2016-01-07 14:56:55 +0800

    use JS DOM eventListener to change the color of the tweet button

--- public/asset/js/myScript.js
+++ public/asset/js/myScript.js
@@ -1,11 +1,13 @@
-document.getElementsByClassName("global_tweet_button")[0].onmouseover = onmouseoverChangeGloTetBtnBgdCol;
-document.getElementsByClassName("global_tweet_button")[0].onmouseout = onmouseoutChangeGloTetBtnBgdCol;
+var tweet_button = document.getElementsByClassName("global_tweet_button")[0];
 
-function onmouseoverChangeGloTetBtnBgdCol() {
+tweet_button.addEventListener("mouseover", global_tweet_mouseOver);
+tweet_button.addEventListener("mouseout", global_tweet_mouseOut);
+
+function global_tweet_mouseOver() {
 	this.style.background = "#0084cc";
 }
 
-function onmouseoutChangeGloTetBtnBgdCol() {
+function global_tweet_mouseOut() {
 	this.style.background = "#0084B4";
 }
 
